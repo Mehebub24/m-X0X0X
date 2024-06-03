12900d5c8dc42d7352868555cc2d33831ea32184
Mehebub24 <Mehebubkalapati@gmail.com>
2024-06-03 22:52:57 +0530

    scripting

--- script.js
+++ script.js
@@ -13,21 +13,22 @@ const changeTurn = () =>{
 const checkWin = () =>{
     let boxtext = document.getElementsByClassName('boxtext')
     let wins = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
+        [0, 1, 2, 5, 5, 0],
+        [3, 4, 5, 5, 15, 0],
+        [6, 7, 8, 5, 25, 0],
+        [0, 3, 6, -5, 15, 90],
+        [1, 4, 7, 5, 15, 90],
+        [2, 5, 8, 15, 15, 90],
+        [0, 4, 8, 5, 15, 45],
+        [2, 4, 6, 5, 15, 135],
     ]
     wins.forEach(e=>{
-        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !==" " )){
-            document.querySelector('.info').innerText = boxtext[e[0]].innerText + "Won"
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !=="" )){
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
             isGameOver = true  
-            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "160px"
-            
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px"
+            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
+            document.querySelector(".line").style.width = "20vw";
         }
     })
 }
@@ -44,8 +45,21 @@ Array.from(boxes).forEach(element=>{
             turnMusic.play();
             checkWin();
             if(!isGameOver){
-                document.getElementsByClassName("info")[0].innerHTML= "Trun for" + turn
+                document.getElementsByClassName("info")[0].innerHTML= "Trun for " + turn
             }
         }
     })
+})
+
+//reset button
+
+reset.addEventListener('click',()=>{
+    let boxtexts = document.querySelectorAll('.boxtext')
+    Array.from(boxtexts).forEach(element =>{
+        element.innerHTML = ""
+    })
+    turn="X";
+    isGameOver = false;
+    document.getElementsByClassName("info")[0].innerHTML= "Trun for " + turn
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width ="0px"
 })
